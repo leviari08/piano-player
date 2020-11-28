fa90163805cd1f1b75f04b768abe764efe88cd77
Ariel Levitas <leviari08@gmail.com>
2020-11-28 11:36:35 +0200

    more util functions

--- PianoPlayer/Models/Utils.cs
+++ PianoPlayer/Models/Utils.cs
@@ -1,13 +1,35 @@
 ﻿using System;
-
+using System.Linq;
 
 namespace PianoPlayer
 {
-    static class Utils
+    public static class Utils
     {
         public static string nSpaces(int n)
         {
             return new string(' ', n);
+        }
+
+        public static string ExtendEachCharWith(this string s, string mix)
+        {
+            string res = "";
+            foreach (char c in s)
+            {
+                res += c + mix;
+            }
+            return res;
+        }
+
+        public static string Repeat(this string s, int times)
+        {
+            //return string.Concat(Enumerable.Repeat(s, times));
+
+            string res = "";
+            for (int i = 0; i < times; i++)
+            {
+                res += s;
+            }
+            return res;
         }
     }
 }
